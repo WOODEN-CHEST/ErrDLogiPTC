5f5287a3267457d5ab56e7d92d3465f613bc3d6c
wooden_chest <cernavskiskristofers@gmail.com>
2026-07-04 17:02:35 +0300

    added fonts and models.

---
+++ include/GameModel.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "raylib/raylib.h"
+
+typedef struct GameModelStruct
+{
+    Model _rayModel;
+} GameMode;
+
+typedef struct GameMeshStruct
+{
+    Mesh _rayMesh;
+} GameMesh;
